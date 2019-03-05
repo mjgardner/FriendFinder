fc6dac19d85800de24e2f6c57744fb4e604d7b35
Mark Gardner <mark.gardner@endurance.com>
2019-03-05 09:58:30 -0600

    add JSON middleware

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var express = require("express"),
 var path = require("path");
 var PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 require(path.join(__dirname, "app", "routing", "htmlRoutes"))(app);
 require(path.join(__dirname, "app", "routing", "apiRoutes"))(app);
 
