53f54ecbaeac1bc99710502956c44a9b966b5f4c
Mark Gardner <mark.gardner@endurance.com>
2019-03-05 10:04:04 -0600

    friends API GET and POST; TODO: compatibility logic and return from POST

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,31 @@
 var path = require("path");
+var fs = require("fs");
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
-    // TODO: display a JSON of all possible friends
+    res.sendFile(path.join(__dirname, "..", "data", "friends.js"));
   });
 
   app.post("/api/friends", function(req, res) {
-    // TODO: handle incoming survey results and compatibility logic
+    fs.readFile(
+      path.join(__dirname, "..", "data", "friends.js"),
+      { encoding: "utf8" },
+      function(err, data) {
+        if (err) res.sendStatus(500);
+
+        var friends = JSON.parse(data);
+        if (!Array.isArray(friends)) res.sendStatus(500);
+        friends.push(req.body);
+        fs.writeFile(
+          path.join(__dirname, "..", "data", "friends.js"),
+          JSON.stringify(friends),
+          function(err) {
+            if (err) res.sendStatus(500);
+
+            // TODO: compatibility logic
+          }
+        );
+      }
+    );
   });
 };
