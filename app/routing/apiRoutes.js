2c8e38fbf21e512fe6925e38762090a0c6151edc
Mark Gardner <mark.gardner@endurance.com>
2019-03-05 18:26:47 -0600

    handle POST

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,7 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
     fs.readFile(
       path.join(__dirname, "..", "data", "friends.js"),
       { encoding: "utf8" },
@@ -15,14 +16,33 @@ module.exports = function(app) {
 
         var friends = JSON.parse(data);
         if (!Array.isArray(friends)) res.sendStatus(500);
-        friends.push(req.body);
+
+        var leastTotalDifference = (5 - 1) * 10 + 1;
+        var closestFriend = { name: "", photo: "" };
+        friends.forEach(function(friend) {
+          var differences = [];
+          for (var i = 0; i < newFriend.scores.length; i++) {
+            differences.push(Math.abs(friend.scores[i] - newFriend.scores[i]));
+          }
+          var totalDifference = differences.reduce(function(
+            previousValue,
+            currentValue
+          ) {
+            return previousValue + currentValue;
+          });
+          if (totalDifference < leastTotalDifference) {
+            leastTotalDifference = totalDifference;
+            closestFriend = friend;
+          }
+        });
+        res.json({ name: closestFriend.name, photo: closestFriend.photo });
+
+        friends.push(newFriend);
         fs.writeFile(
           path.join(__dirname, "..", "data", "friends.js"),
           JSON.stringify(friends),
           function(err) {
             if (err) res.sendStatus(500);
-
-            // TODO: compatibility logic
           }
         );
       }
