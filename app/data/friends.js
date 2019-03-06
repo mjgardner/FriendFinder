6cb30614e3e2fbbd2f7d357f9f0566efd73c0070
Mark Gardner <mark.gardner@endurance.com>
2019-03-05 18:26:31 -0600

    add another sample friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,1 @@
-[{"name":"Mark Gardner","photo":"https://mjgardner.github.io/assets/images/38495672_10155549849255779_5660012240309321728_n.jpg","scores":["2","3","3","2","4","3","3","4","4","2"]}]
+[{"name":"Mark Gardner","photo":"https://mjgardner.github.io/assets/images/38495672_10155549849255779_5660012240309321728_n.jpg","scores":["2","3","3","2","4","3","3","4","4","2"]},{"name":"Peter Winston","photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1200px-Apple_logo_black.svg.png","scores":["4","3","3","4","3","3","4","3","3","3"]}]
